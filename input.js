f5fc1f8eab4488d3d73a41862fabf23e600d78bc
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2022-12-20 00:25:56 +0000

    day1 solved

---
+++ input.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+
+var input = fs.readFileSync('./input.txt')
+
+var data = input.toString ()
+
+var splitData = data.split ('\n')
+
+var currentElfTotal=0;
+var allElfTotals=[];
+
+// splitData.forEach (element => {
+//   if (element == ""){
+//   allElfTotals.push(currentElfTotals);
+//   currentElfTotals=0;
+// }
+// else {
+//   currentElfTotals=parseInt(element);
+// }});
+
+console.log (splitData)
+
+for (let index = 0; index < splitData.length; index++) {
+  var line = splitData [index];
+  if (line == "") {
+    allElfTotals.push(currentElfTotal);
+    currentElfTotal=0;
+  }
+  else { 
+    currentElfTotal = currentElfTotal + parseInt(line);
+}};
+
+
+console.log (allElfTotals)
+
+var greedyElf = 0
+
+function getGreatest(allElfTotals) {
+  if (greedyElf < allElfTotals) {
+      greedyElf = allElfTotals;
+  }
+}
+
+allElfTotals.forEach(getGreatest)
+
+console.log (greedyElf)
+
+// console.log (input)
+// console.log (splitData)
+// console.log (allElfTotals)
